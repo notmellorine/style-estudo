d9ff258e21c10be89b57ba20f769740b7d69df78
Aryadne Coppini <arya.kusse@gmail.com>
2023-02-27 18:27:54 -0300

    Atualização do código para fácil compreensão da ordem dos containeres.

--- App.js
+++ App.js
@@ -1,155 +1,194 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.containerApp}>
-      <View style={styles.container1}>
-        <View>
-          <Text style={styles.Title}>Geography</Text>
+    <View style={styles.containerGeral}>
+      <View style={styles.containerTitle}>
+        <Text style={styles.title}>Geography</Text>
+      </View>
+      <View>
+        <TextInput style={[styles.textInput, styles.shadowProps]} placeholder='Search' placeholderTextColor='white'/>
+      </View>
+      <View style={styles.containerGeography}>
+        <View style={styles.circle1}/>
+        <View style={styles.textContainer}>
+          <Text style={styles.textItens}>Geography</Text>
+          <Text style={styles.subtitleItens}>Something etc etc...</Text>
         </View>
-        <View>
-          <TextInput style={styles.textInput} placeholder='Search' placeholderTextColor='white'/>
+      </View>
+      <View style={styles.containerSpace}>
+        <View style={styles.circle2}/>
+        <View style={styles.textContainer}>
+          <Text style={styles.textShortItens}>Space</Text>
+          <Text style={styles.subtitleItens}>Something etc etc...</Text>
         </View>
-          <View style={styles.columnContainer}>
-              <View style={styles.circle1}/>
-              <View style={styles.columns}>
-              <Text style={styles.columnsText}>Geography</Text>
-              <Text style={styles.fadingText}>Exemplo etc e tal...</Text>
-            </View>
-          </View>
-          <View style={styles.columnContainer}>
-              <View style={styles.circle2}/>
-              <View style={styles.columns}>
-              <Text style={styles.columnsText}>Space</Text>
-              <Text style={styles.fadingText}>Exemplo etc e tal...</Text>
-            </View>
-          </View>
-          <View style={styles.columnContainer}>
-              <View style={styles.circle3}/>
-              <View style={styles.columns}>
-              <Text style={styles.columnsText}>Place</Text>
-              <Text style={styles.fadingText}>Exemplo etc e tal...</Text>
-            </View>
-          </View>
-          <View style={styles.columnContainer}>
-              <View style={styles.circle4}/>
-              <View style={styles.columns}>
-              <Text style={styles.columnsText}>Environment</Text>
-              <Text style={styles.fadingText}>Exemplo etc e tal...</Text>
-            </View>
-          </View>
-          <View style={styles.columnContainer}>
-              <View style={styles.circle5}/>
-              <View style={styles.columns}>
-              <Text style={styles.columnsText}>Sustainability</Text>
-              <Text style={styles.fadingText}>Exemplo etc e tal...</Text>
-            </View>
-          </View>
+      </View>
+      <View style={styles.containerPlace}>
+        <View style={styles.circle3}/>
+        <View style={styles.textContainer}>
+          <Text style={styles.textShortItens}>Place</Text>
+          <Text style={styles.subtitleItens}>Something etc etc...</Text>
         </View>
-        <View style={styles.buttonsRow}>
-          <View style={styles.buttons}></View>
-          <View style={styles.buttons}></View>
-          <View style={styles.buttons}></View>
-          </View>
+      </View>
+      <View style={styles.containerEnvironment}>
+        <View style={styles.circle4}/>
+        <View style={styles.textContainer}>
+          <Text style={styles.textItens}>Environment</Text>
+          <Text style={styles.subtitleItens}>Something etc etc...</Text>
+        </View>
+      </View>
+      <View style={styles.containerSustainability}>
+        <View style={styles.circle5}/>
+        <View style={styles.textContainer}>
+          <Text style={styles.textItens}>Sustainability</Text>
+          <Text style={styles.subtitleItens}>Something etc etc...</Text>
+        </View>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.buttons}>
+          <Text style={styles.textButtons}>🗑️</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttons}>
+          <Text style={styles.textButtons}>✎</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttons}>
+          <Text style={styles.textButtons}>☆</Text>
+        </TouchableOpacity>
+      </View>
       </View>
   );
 }
 
 const styles = StyleSheet.create({
-  containerApp: {
-    flex: 1,
-    backgroundColor: '#333333',
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  container1: {
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  columnContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap'
-  },
-  Title: {
-    margin: 50,
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: 'white'
-  },
-  textInput: {
-    backgroundColor: '#4d4d4d',
-    fontSize: 20,
-    padding: 23,
-    margin: 50,
-    color: 'white',
-    width: 550,
-    borderRadius: 100
-  },
-  circle1: {
- 
-    backgroundColor: '#03ced8',
-    borderRadius: 100,
-    flexDirection: 'column',
-    width: 120,
-    height: 120
-  },
-  circle2: {
-    backgroundColor: '#624adc',
-    borderRadius: 100,
-    flexDirection: 'row',
-    width: 120,
-    height: 120
-  },
-  circle3: {
-    backgroundColor: '#ffca00',
-    borderRadius: 100,
-    flexDirection: 'row',
-    width: 120,
-    height: 120
-  },
-  circle4: {
-    backgroundColor: '#30d40c',
-    borderRadius: 100,
-    flexDirection: 'row',
-    width: 120,
-    height: 120
-  },
-  circle5: {
-    backgroundColor: '#fe3376',
-    borderRadius: 100,
-    flexDirection: 'row',
-    width: 120,
-    height: 120
-  },
-  columns: {
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: 40
-  },
-  columnsText: {
-    fontSize: 25,
-    alignItems: 'center',
-    color: 'white',
-    marginRight: 120,
-    
-  },
-  fadingText: {
-    alignItems: 'center',
-    color: '#5c5c5c',
-    fontSize: 15,
-    marginRight: 110
-  },
-  buttonsRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-around'
-  },
-  buttons: {
-    alignItems: 'center',
-    width: 100,
-    height: 100,
-    borderRadius: 100,
-    backgroundColor: '#4c4c4c',
-    justifyContent: 'space-around',
-    marginLeft: 120
-  }
+    containerGeral: {
+      flex: 1,
+      backgroundColor: '#33342f'
+    },
+    containerTitle: {
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      margin: 10
+    },
+    title: {
+      color: 'white',
+      fontSize: 50,
+      fontWeight: 'bold',
+      margin: 10
+    },
+    textInput: {
+      backgroundColor: '#4d4d4d',
+      padding: 20,
+      fontSize: 20,
+      borderRadius: 100,
+      marginHorizontal: 50,
+      color: 'white'
+    },
+    shadowProps: {
+      elevation: 20
+    },
+    containerGeography: {
+      alignItems: 'center',
+      flexDirection: 'row',
+      
+    },
+    circle1: {
+      backgroundColor: '#03ced8',
+      borderRadius: 100,
+      width: 120,
+      height: 120,
+      margin: 45,
+      alignSelf: 'flex-start'
+    },
+    textContainer: {
+      flexDirection: 'column'
+    },
+    textItens: {
+      color: 'white',
+      fontSize: 30,
+      marginHorizontal: 75,
+      alignSelf: 'center',
+      flexDirection: 'column'
+    },
+    subtitleItens: {
+      alignSelf: 'center',
+      color: '#5b5b5b',
+      flexDirection: 'column'
+    },
+    containerSpace: {
+      alignItems: 'center',
+      flexDirection: 'row',
+    },
+    circle2: {
+      backgroundColor: '#5c3cfe',
+      borderRadius: 100,
+      width: 120,
+      height: 120,
+      margin: 45,
+      marginTop: -10,
+      alignSelf: 'flex-start'
+    },
+    circle3: {
+      backgroundColor: '#fdc902',
+      borderRadius: 100,
+      width: 120,
+      height: 120,
+      margin: 45,
+      marginTop: -10,
+      alignSelf: 'flex-start'
+    },
+    circle4: {
+      backgroundColor: '#2cce02',
+      borderRadius: 100,
+      width: 120,
+      height: 120,
+      margin: 45,
+      marginTop: -10,
+      alignSelf: 'flex-start'
+    },
+    circle5: {
+      backgroundColor: '#ea2d6b',
+      borderRadius: 100,
+      width: 120,
+      height: 120,
+      margin: 45,
+      marginTop: -10,
+      alignSelf: 'flex-start'
+    },
+    containerPlace: {
+      alignItems: 'center',
+      flexDirection: 'row',
+    },
+    containerEnvironment: {
+      alignItems: 'center',
+      flexDirection: 'row',
+    },
+    containerSustainability: {
+      alignItems: 'center',
+      flexDirection: 'row'
+    },
+    textShortItens: {
+      color: 'white',
+      fontSize: 30,
+      marginHorizontal: 115,
+      alignSelf: 'center',
+      flexDirection: 'column'
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+    },
+    buttons: {
+      backgroundColor: "#4d4d4d", 
+      width:100, 
+      height: 100, 
+      borderRadius: 100,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    textButtons:{
+      color: 'white',
+      fontSize: 50
+    }
 });
